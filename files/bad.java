4cc08c4b9d0b1701e95dd52d36cccc81e13ea442
varuuuun <72968318+varuuuun@users.noreply.github.com>
2026-02-17 16:35:38 +0800

    Delete test files

--- files/bad.java
+++
@@ -1,8 +0,0 @@
-public class myClass{
-public void RenderUI(){
-int a=1; int b=2;
-if(isValid){
-doSomething(); // Comment longer than usual to test line length checking functionality in the static analysis module of the code style reviewer tool.
-}
-}
-}
--- files/good.java
+++
@@ -1,7 +0,0 @@
-public class MyClass {
-
-    public void renderUI() {
-        int a = 1;
-        int b = 2;
-    }
-}
